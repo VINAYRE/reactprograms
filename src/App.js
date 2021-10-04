d6f3d7ffc049ad9d94a2d60aefb71ee67f518483
VINAYRE <vr9708582@gmail.com>
2021-10-03 23:44:31 -0700

    okie

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from "react";
-import Message from "./states/Message"
+
 import Bootstrap from "./states/Bootstrap";
-import Table from "./states/Table";
+import Table from "./states/Table"; 
+
+
+import One from './props/One';
+import Event from './EventHandler/Event'
+import Binding from "./EventBinding/Binding";
+import Form from "./formHandler/Form";
+import Digitalwatch from "./Lifecycle/Digitalwatch";
 class App extends React.Component{
     render(){
     return(
         <>
-        <Message/> 
+       
         <hr/><hr/>
         <Bootstrap/>
         <hr/><hr/>
-        <Table/>
+        <Table/> 
+
+
+        <marquee bgcolor='yellow' >PROPS</marquee> 
+        <One/>
+        <hr/><hr/>
+         <marquee bgcolor="blue" textcolor="white">welcome to EventHandler</marquee> 
+        <hr/><hr/>
+        <Event/>
+        <hr/><hr/>
+        <marquee bgcolor='red' >welcome to EventBinding</marquee> 
+        <Binding/> 
+        <marquee bgcolor='green' >formHandler</marquee> 
+        <Form/>
+        <hr/><hr/>
+        <Digitalwatch/>
         </>
     )
 }
